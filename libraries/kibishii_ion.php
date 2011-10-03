f3fefde7a821180a0d138cc28ce7b5dfc03638c3
Ian Chen <github@bubblemix.net>
2011-10-03 23:06:08 +0900

    added kibishii ion roles library

---
+++ libraries/kibishii_ion.php
@@ -0,0 +1,21 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class kibishii_ion {
+	function get_roles($user_id) {
+		$authorization_class = 'Ion_auth_model';
+		$authorization_method = 'get_users_groups';
+		$authorization_class_file = 'models/ion_auth_model.php';
+		
+		if ( ! class_exists($authorization_class)) {
+			require_once(APPPATH.$authorization_class_file);
+		}
+		$auth_object = new $authorization_class;
+		$roles = $auth_object->$authorization_method();
+		$role_names = array();
+		foreach ($roles as &$role) {
+		    $role_names[] = $role->name;
+		}			
+		return $role_names;
+	}
+}
+
+?>
